24b8a5090234ba8d1ac91b37d45c3b358c545ba7
ZagZx <zagzcraft@gmail.com>
2024-07-18 16:21:33 -0300

    scripts

---
+++ calc.js
@@ -0,0 +1,21 @@
+function getjson(){
+    return fetch('./prices.json')
+    .then(response => {
+        
+        return response.json();
+    })
+    
+    
+}
+
+async function calcular(){
+    var milhas = document.getElementById('milhas')
+    var reais = document.getElementById('reais')
+    var jsonf = await getjson();
+    
+    var calculo = (jsonf['azul'] * (milhas.value /1000)).toFixed(2)
+    
+    reais.value = 'R$ ' + String(calculo)
+    
+    
+}
