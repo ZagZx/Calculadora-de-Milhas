c0491015ffd58f7b0c57211d40b100ddcb06cd16
ZagZx <zagzcraft@gmail.com>
2024-07-18 23:21:11 -0300

    erro comp

--- calc.js
+++ calc.js
@@ -7,13 +7,30 @@ function getjson(){
     
     
 }
+function piscar_cor(){
+    milhas.style.color = 'black'
+    
+
+}
 
 async function calcular(){
-    var milhas = document.getElementById('milhas')
-    var reais = document.getElementById('reais')
-    var jsonf = await getjson();
+    const milhas = document.getElementById('milhas')
+    const reais = document.getElementById('reais')
+    const select = document.getElementById('companhia')
+
+    if (select.value != "nulo"){
+        const jsonf = await getjson();
     
-    var calculo = (jsonf['azul'] * (milhas.value /1000)).toFixed(2)
-    
-    reais.value = 'R$ ' + String(calculo)
+        var calculo = (jsonf[select.value] * (milhas.value /1000)).toFixed(2)
+        
+        reais.value = 'R$ ' + String(calculo)
+
+    }
+    else{
+        milhas.style.color = 'red'
+        setTimeout(piscar_cor,1000)
+        
+        
+       
+    }
 }
