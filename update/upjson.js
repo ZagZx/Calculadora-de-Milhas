3c28b15a62bdff1a0b223c54fe01eb40ce12aa54
ZagZx <zagzcraft@gmail.com>
2024-07-26 15:51:38 -0300

    fim

--- update/upjson.js
+++ update/upjson.js
@@ -9,43 +9,61 @@ function getjson(){
 }
 //FALTA FAZER A AUTENTICAÇÃO NO FIREBASE
 async function postjson(){
-    const latam = document.getElementById('latam_input')
+
+    const latam = document.getElementById('latam_input') 
     const gol = document.getElementById('gol_input')
     const azul = document.getElementById('azul_input')
 
-    const prices = {"latam":Number(latam.value),"gol":Number(gol.value), "azul": Number(azul.value)}
+    
+    //"latam":Number(latam.value),"gol":Number(gol.value), "azul": Number(azul.value)
+    var prices = {}
+
+    if (latam.value != ''){
+        prices['latam'] = Number(latam.value)
+    }
+
+    if (gol.value != ''){
+        prices['gol'] = Number(gol.value)
+    }
+
+    if (azul.value != ''){
+        prices['azul'] = Number(azul.value)
+    }
 
     
-    
 
-    
-    fetch('https://calculadora-de-milhas-aef3f-default-rtdb.firebaseio.com/.json')
-    .then(response => {return response.json()})
-    .then(jsonData =>{
-        for (var comp in prices){
-            if (prices[comp] != ''){
-                switch (comp){
-                    case 'latam':
+    if (Object.keys(prices).length != 0){
+        fetch('https://calculadora-de-milhas-aef3f-default-rtdb.firebaseio.com/.json')
+        .then(response => {return response.json()})
+        .then(jsonData =>{
+            
+            for (var comp in prices){
+                if (prices[comp] != 0){
+                    if (comp == 'latam'){
                         jsonData.latam = prices[comp]
-                    
-                    case 'gol':
+                    }
+                    else if (comp == 'gol'){
                         jsonData.gol = prices[comp]
-                    
-                    case 'azul':
+                    }
+                    else if (comp == 'azul'){
                         jsonData.azul = prices[comp]
+                    }
                     
                 }
-                
             }
-        }
-        console.log(jsonData)
-        return fetch('https://calculadora-de-milhas-aef3f-default-rtdb.firebaseio.com/.json', {
-            method: 'PATCH',
-            headers: {'Content-Type':'application/json'},
-            body: JSON.stringify(jsonData)
+            console.log(jsonData)
+            return fetch('https://calculadora-de-milhas-aef3f-default-rtdb.firebaseio.com/.json', {
+                method: 'PATCH',
+                headers: {'Content-Type':'application/json'},
+                body: JSON.stringify(jsonData)
+            })
+            
         })
-    })
-        
+    }
+    else{
+        console.log('Sem alterações')
+    }
     
         
 }
+
